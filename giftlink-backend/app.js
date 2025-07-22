002d4fbc36d517d7d560fdfc741b0b874a2ca90c
sasha55549 <sasha.kozachok@gmail.com>
2025-07-22 07:32:53 -0400

    Added gift routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -8,6 +8,7 @@ const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
 
+
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -23,7 +24,8 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
+app.use('/api/gifts',giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
