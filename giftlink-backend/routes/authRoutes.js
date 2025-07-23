4914b4a0a164e7a600c4274af20f37751a28a45a
sasha55549 <sashakozwork@gmail.com>
2025-07-23 07:05:47 -0400

    Added login route

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -22,6 +22,10 @@ router.post('/register', async (req, res) => {
          const collection = await db.collection('users');
         //Task 3: Check for existing email
         const existingEmail = await collection.findOne({email: req.body.email});
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
@@ -44,4 +48,34 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
+
+router.post('/login', async (req,res)=>{
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection('users');
+        const theUser = await collection.findOne({email: req.body.email});
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password,theUser.password);
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({error: 'Wrong password'});
+            }
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            const payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            const authtoken = jwt.sign(payload,JWT_SECRET);
+            res.json({authtoken,userName,userEmail});
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({error: 'User not found'});
+        }
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
